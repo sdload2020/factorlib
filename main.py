08dfdcce552714936d69b37712f715ff88ffb3fd
yangzl15 <14996159+yangzl15@user.noreply.gitee.com>
2025-06-19 12:14:21 +0800

    增加if_crontab为true和run_mode为online下，执行因子成功flag文件

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 # # main.py
 import subprocess
 import argparse
-import sys
+import ast,sys
 import yaml
 import multiprocessing
 import os
 import time
 from calc_alpha import AlphaCalc
-from configs.syspath import (BASE_PATH, WORK_PATH, LOGS_PATH)
+from configs.syspath import (BASE_PATH, WORK_PATH, LOGS_PATH, FACTOR_CODE_PATH)
 from loguru import logger
 from utils.logger_setup import setup_execution_logger
+from datetime import datetime
+from pathlib import Path
 
+def write_flag_file(factor_name):
+    today = datetime.now().strftime("%Y-%m-%d")
+    # today = "2025-06-18"
+    flag_dir = os.path.join(WORK_PATH, 'flags', today)
+    os.makedirs(flag_dir, exist_ok=True)
+    open(os.path.join(flag_dir, f"{factor_name}.flag"), 'w').close()
+
+def get_factor_params(factor_name):
+    fileName = factor_name + '.py'
+    path = Path(FACTOR_CODE_PATH)
+    for file_path in path.rglob('*.py'):  # 使用 rglob 递归匹配所有文件
+        if file_path.is_file():
+            if(file_path.name == fileName):
+                with open(file_path, 'r',encoding='utf-8') as f:
+                    source = f.read()
+                tree = ast.parse(source)
+                arrays = {}
+
+                for node in tree.body:
+                    if isinstance(node, ast.Assign):
+                        for target in node.targets:
+                            if isinstance(target, ast.Name) and target.id == 'config':
+                                arrays = ast.literal_eval(node.value)
+                                break
+    factor_params = next((f for f in arrays['factors'] if f['name'] == factor_name), None)
+    return factor_params
 
 def run_scripts(factor_name):
     # logger.info(BASE_PATH)
@@ -24,6 +52,10 @@ def run_scripts(factor_name):
         return
     subprocess.check_call([sys.executable, os.path.join(WORK_PATH, 'factorlib/')+'run_backtest.py', '--name', factor_name])
     subprocess.check_call([sys.executable, os.path.join(WORK_PATH, 'factorlib/')+'run_plot.py', '--name', factor_name])
+    factor_params = get_factor_params(factor_name)
+    if factor_params.get('if_crontab') and factor_params.get('run_mode') == 'online':
+        write_flag_file(factor_name)
+        logger.info(f"因子 {factor_name} 运行完成, 已写入flag文件...")
 
 def run_scripts2(factor_name):
     # subprocess.check_call([sys.executable, 'factor', '--name', factor_name])
@@ -34,6 +66,10 @@ def run_scripts2(factor_name):
         return
     subprocess.check_call([sys.executable, 'backtest', '--name', factor_name])
     subprocess.check_call([sys.executable, 'plot', '--name', factor_name])
+    factor_params = get_factor_params(factor_name)
+    if factor_params.get('if_crontab') and factor_params.get('run_mode') == 'online':
+        write_flag_file(factor_name)
+        logger.info(f"因子 {factor_name} 运行完成, 已写入flag文件...")
 
 def tmain(names):
     # logger.info("names:"+names)
